bd3de77ddd665c683ed42be07ffd51edf5f09302
Darius Arnold <Darius_A@web.de>
2018-05-02 18:44:21 +0200

    Move documentation of class Game from source to header file

--- Game.cpp
+++ Game.cpp
@@ -4,14 +4,6 @@
 
 Game::Game(){}
 
-/**
- * Spielt ein Spiel "Streichhoelzer". Gibt den Spieler zurueck, der das Spiel gewonnen hat.
- * @param k Maximal erlaubte Anzahl zu entfernender Streichhoelzer pro Zug.
- * @param n Anfaengliche Anzahl Streichhoelzer.
- * @param numberOfPlayers Anzahl menschlicher Spieler.
- * @param computerIsPlayerNo Spielernummer des Computers. Nur definiert beim Spiel gegen den Computer.
- * @return Spieler, der gewonnen hat (1 oder 2). -1 bei Fehlern.
- */
 int Game::playGame(int k, int n, int numberOfPlayers, int computerIsPlayerNo){
 	// ueberpruefe Eingabewerte
 	if (n <= 0 or k<= 0 or n < k or k > n or computerIsPlayerNo != 1 and computerIsPlayerNo != 2) return -1;
@@ -24,15 +16,6 @@ int Game::playGame(int k, int n, int numberOfPlayers, int computerIsPlayerNo){
 	return -1;
 }
 
-/**
- * Spielt einen Zug im Spiel "Streichhoelzer" für den Spieler currentPlayer. Anzahl zu entfernender Streichhoelzer
- * wird abgefragt und von den Gesamtzahl n abgezogen.
- * @param currentPlayer Spielernummer des Spielers, der am Zug ist.
- * @param computerIsPlayerNo Spierlnummer des Computers. Nur definiert beim Spiel gegen den Computer.
- * @param n Aktuelle Anzahl Streichhoelzer vor Beginn des Zuges.
- * @param k Maximal erlaubte Anzahl zu entfernender Streichhoelzer pro Zug.
- * @return Anzahl verbleibender Streichhoelzer nach dem Zug.
- */
 int Game::playTurn(int &currentPlayer, int computerIsPlayerNo, int &n, int k){
 	int maxNumber = std::min(n, k);
 	std::cout << "Spieler " << currentPlayer << ", wie viele Streichhoelzer " << "(1..." << maxNumber << ") " << "sollen entfernt werden?" << std::endl;
@@ -43,12 +26,6 @@ int Game::playTurn(int &currentPlayer, int computerIsPlayerNo, int &n, int k){
 	return anzahl;
 }
 
-/**
- * Fragt Spieler nach Eingabe wie viele Streichhoelzer entfernt werden sollen. Valide Eingaben sind Integer zwischen 1 und maxNumber.
- * Bei einer invaliden Eingabe wird die Eingabe wiederholt.
- * @param maxNumber Groesste gueltige Benutzereingabe.
- * @return Anzahl zu entfernender Streichhoelzer.
- */
 int Game::getUserAnzahl(int maxNumber){
 	int anzahl;
 	while (true){
@@ -65,19 +42,11 @@ int Game::getUserAnzahl(int maxNumber){
 	return anzahl;
 }
 
-/**
- * Gibt Anzahl der Streichhoelzer zurueck, die in einem bestmoeglichen Spielzug entfernt werden muss.
- * @param maxNumber Maximal zu entfernende Anzahl Streichhoelzer.
- * @return Anzahl zu entfernender Streichhoelzer.
- */
 int Game::getComputerAnzahl(int n, int k){
 	int result = (n-1) % (k+1);
 	return result;
 }
 
-/**
- * Subroutine um den aktuellen Spieler zu wechseln. Wechselt currentPlayer zwischen 1 und 2.
- */
 void Game::ToggleCurrentPlayer(int &currentPlayer){
 	currentPlayer = 2 / currentPlayer;
 }
--- Game.h
+++ Game.h
@@ -3,22 +3,52 @@
 
 #include "GameBase.h"
 
-/* 
-		Implementiert hier eure Klasse Game, die von Klasse GameBase erbt und
-		MINDESTENS ihre Funktion 
-		int playGame(int k, int n, int numberOfPlayers, int computerIsPlayerNo)
-		implementiert!
-*/
+/**
+ * Game implementiert das Nim-Spiel in 1D. Um zu spielen muss eine Instanz erstellt werden und deren playGame-Funktion aufgerufen
+ * werden. Bei regulärem Spielablauf gibt sie den Spieler, der gewonnen hat zurück, sonst -1.
+ */
 class Game : public GameBase{
 public:
 	Game();
+	/**
+	 * Spielt ein Spiel "Streichhoelzer". Gibt den Spieler zurueck, der das Spiel gewonnen hat.
+	 * @param k Maximal erlaubte Anzahl zu entfernender Streichhoelzer pro Zug.
+	 * @param n Anfaengliche Anzahl Streichhoelzer.
+	 * @param numberOfPlayers Anzahl menschlicher Spieler.
+	 * @param computerIsPlayerNo Spielernummer des Computers. Nur definiert beim Spiel gegen den Computer.
+	 * @return Spieler, der gewonnen hat (1 oder 2). -1 bei Fehlern.
+	 */
 	int playGame(int k, int n, int numberOfPlayers, int computerIsPlayerNo);
 private:
+	/**
+	 * Fragt Spieler nach Eingabe wie viele Streichhoelzer entfernt werden sollen. Valide Eingaben sind Integer zwischen 1 und maxNumber.
+	 * Bei einer invaliden Eingabe wird die Eingabe wiederholt.
+	 * @param maxNumber Groesste gueltige Benutzereingabe.
+	 * @return Anzahl zu entfernender Streichhoelzer.
+	 */
 	int getUserAnzahl(int maxNumber);
+
+	/**
+	 * Gibt Anzahl der Streichhoelzer zurueck, die in einem bestmoeglichen Spielzug entfernt werden muss.
+	 * @param maxNumber Maximal zu entfernende Anzahl Streichhoelzer.
+	 * @return Anzahl zu entfernender Streichhoelzer.
+	 */
 	int getComputerAnzahl(int n, int k);
+
+	/**
+	 * Subroutine um den aktuellen Spieler zu wechseln. Wechselt currentPlayer zwischen 1 und 2.
+	 */
 	void ToggleCurrentPlayer(int &currentPlayer);
-	int userTurn(int playerNo, int maxNumber);
-	int computerTurn(int playerNo, int n, int k, int maxNumber);
+
+	/**
+	 * Spielt einen Zug im Spiel "Streichhoelzer" für den Spieler currentPlayer. Anzahl zu entfernender Streichhoelzer
+	 * wird abgefragt und von den Gesamtzahl n abgezogen.
+	 * @param currentPlayer Spielernummer des Spielers, der am Zug ist.
+	 * @param computerIsPlayerNo Spierlnummer des Computers. Nur definiert beim Spiel gegen den Computer.
+	 * @param n Aktuelle Anzahl Streichhoelzer vor Beginn des Zuges.
+	 * @param k Maximal erlaubte Anzahl zu entfernender Streichhoelzer pro Zug.
+	 * @return Anzahl verbleibender Streichhoelzer nach dem Zug.
+	 */
 	int playTurn(int &currentPlayer, int computerIsPlayerNo, int &n, int k);
 };
 
